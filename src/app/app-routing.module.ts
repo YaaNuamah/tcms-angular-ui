1e4c8c676e05ac0122cd9dffbb16ae781043431d
YaaNuamah <yaa_fordjour@hotmail.com>
2019-12-03 17:13:09 +0000

    components routed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomersComponent } from './customers/customers.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ReportsComponent } from './reports/reports.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'customers',component: CustomersComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: '', redirectTo: '/customers', pathMatch: 'full'},
+  {path: 'reports', component: ReportsComponent},
+  {path: 'nav', component: NavComponent},
+  {path: 'home', component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ReportsComponent } from './reports/reports.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent,
+    ProjectsComponent,
+    ReportsComponent,
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
