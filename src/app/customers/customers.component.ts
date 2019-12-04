3fdeadf6ca38dca3de08abbe9a475f433642d446
YaaNuamah <yaa_fordjour@hotmail.com>
2019-12-04 10:22:37 +0000

    added json format to customers

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomersComponent implements OnInit {
 
+  customers = [
+    {id: '1', FirstName: 'Mandy', LastName: 'Lou', Contact: "0273458902", Address: '123 Mustard Street', Email: 'mandyl@io.com'},
+    {id: '2', FirstName: 'Stephen', LastName: 'Hawkings', Contact: '0543237894', Address: '34 Dzorwulu Avenue', Email: 'stephenh@net.com'},
+    {id: '3', FirstName: 'Lucy', LastName: 'Goodman', Contact: '001456892345', Address: '56 Cantoment Avenue Street', Email: 'goodmanl@gmail.com'}
+  ]
+
   constructor() { }
+
 
   ngOnInit() {
   }
